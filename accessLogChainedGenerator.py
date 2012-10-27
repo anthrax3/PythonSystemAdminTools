083440aa769ce00a43a484050d1f604912b79280
bigsnarfdude <ohprecio@gmail.com>
2012-10-27 11:35:27 -0400

    refactored - Counter takes an iterable

--- accessLogChainedGenerator.py
+++ accessLogChainedGenerator.py
@@ -1,8 +1,7 @@
 from collections import Counter
-cnt = Counter()
 
 wwwlog = open('access.log')
 ip_column = (line.split()[0] for line in wwwlog)
-for word in ip_column: cnt[word] +=1
+cnt = Counter (ip_column)
 for k,v in cnt.items():
     if v > 50: print k,v
